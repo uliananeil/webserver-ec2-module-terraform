61a4fe3687842d71d7b871795346058e74460317
Uliana Neiliuk <uliananeiliuk123@gmail.com>
2023-01-02 20:26:30 +0200

    test

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 terraform {
     backend "s3" {
-    bucket         = "$STATE_BUCKET"
+    bucket         = "state-bucket-134681"
     key            = "terraform.tfstate"
     region  = "eu-central-1"
   }  
