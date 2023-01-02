88d259b3eb2cd74a95e4c38608822b925ef02c56
Uliana Neiliuk <uliananeiliuk123@gmail.com>
2023-01-02 20:32:27 +0200

    test

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,3 +3,10 @@ provider "aws" {
   region = "eu-central-1"
 }
 
+terraform {
+    backend "s3" {
+    bucket         = "state-bucket-134681"
+    key            = "terraform.tfstate"
+    region  = "eu-central-1"
+  }  
+}
