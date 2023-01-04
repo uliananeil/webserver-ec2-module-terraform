3bf42c6cce58ff4398f43ef96cb69a3a7299b2bf
Uliana Neiliuk <uliananeiliuk123@gmail.com>
2023-01-04 16:06:26 +0200

    delete backend

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,10 +2,4 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-terraform {
-    backend "s3" {
-    bucket         = "terraform-state-bucket-12645"
-    key            = "test/terraform.tfstate"
-    region  = "eu-central-1"
-  }  
-}
+
